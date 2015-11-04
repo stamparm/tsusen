02b4d16452c8cb446bd68343626c6059fb34a6a8
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-11-04 13:49:57 +0100

    New module

---
+++ core/common.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2015 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+
+def addr_to_int(value):
+    _ = value.split('.')
+    return (long(_[0]) << 24) + (long(_[1]) << 16) + (long(_[2]) << 8) + long(_[3])
+
+def int_to_addr(value):
+    return '.'.join(str(value >> n & 0xFF) for n in (24, 16, 8, 0))
+
+def make_mask(bits):
+    return 0xffffffff ^ (1 << 32 - bits) - 1
